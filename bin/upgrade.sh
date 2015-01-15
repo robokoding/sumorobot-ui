baa0f6a9fe979ff329e1b70cdd3081cbc31996a1
Silver Kuusik <silver.kuusik@gmail.com>
2015-01-15 07:08:08 +0100

    updated to default IP

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl --request POST http://sumorobot:@${1:-192.168.0.102}/data_success.html -F "CMD=WEB_UPLOAD" -F "files=@dist/sumorobot.bin"
+curl --request POST http://sumorobot:@${1:-10.10.100.254}/data_success.html -F "CMD=WEB_UPLOAD" -F "files=@dist/sumorobot.bin"
