2eda525fdf1ddcb188bd77f792ce4d313b148772
Silver Kuusik <silver.kuusik@gmail.com>
2015-01-18 00:55:38 +0100

    Update upgrade.sh

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl --request POST http://sumo@${1:-10.10.100.254}/data_success.html -F "CMD=WEB_UPLOAD" -F "files=@dist/sumorobot.bin"
+curl --request POST http://sumo:@${1:-10.10.100.254}/data_success.html -F "CMD=WEB_UPLOAD" -F "files=@dist/sumorobot.bin"
